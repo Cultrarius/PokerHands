1cd4f0e23006c28decbead152d5815d3ecb7443b
Michael Galetzka <galetzka@posteo.de>
2016-12-09 17:38:02 +0100

    extended card class

--- src/net/poker/data/Card.java
+++ src/net/poker/data/Card.java
@@ -3,7 +3,7 @@ package net.poker.data;
 /**
  * Defines a single poker card
  */
-public class Card {
+public class Card implements Comparable<Card> {
 
     private Value value;
     private Suit suit;
@@ -22,6 +22,32 @@ public class Card {
 
     public Suit getSuit() {
         return suit;
+    }
+
+    @Override
+    public String toString() {
+        return value + " of " + suit;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Card)) {
+            return false;
+        }
+        Card card = (Card) o;
+        return value == card.value && suit == card.suit;
+    }
+
+    @Override
+    public final int hashCode() {
+        return value.hashCode() + suit.hashCode();
+    }
+
+    @Override
+    public int compareTo(Card card) {
+        int valueComparison = Integer.compare(value.ordinal(), card.value.ordinal());
+        int suitComparison = Integer.compare(suit.ordinal(), card.suit.ordinal());
+        return valueComparison == 0 ? suitComparison : valueComparison;
     }
 
     public enum Value {
